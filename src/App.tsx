2eea8fe2751e78cdd3f1886c79d3129815b30c37
henriits <henritsarents@gmail.com>
2025-06-08 12:13:30 +0300

    feat: add routing and dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+
 function App() {
   return (
     <>
       <header className="justify-center flex flex-col items-center">
         <h1 className="text-4xl font-bold p-8">Smart Learning Tracker</h1>
       </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <div className="container">
+      <h1>Smart Learning Tracker</h1>
+      <p>Welcome back! Here’s your latest learning progress.</p>
+      {/* Future: Add a list of recent logs */}
+    </div>
+  );
+};
+
+export default Dashboard;
